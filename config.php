9c0bf3a937cada3f736d1693b790f5be069e26cf
aliraza34 <89367134+aliraza34@users.noreply.github.com>
2021-08-23 05:51:39 -0700

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,15 @@
 <?php
-$server= "localhost";
+/*$server= "localhost";
 $u_name= "root";
 $password= "";
 $db= "inter_db";
+*/
+
+//remort database service
+$server= "remotemysql.com";
+$u_name= "LNyyrkr4e4";
+$password= "XDPW0wJngc";
+$db= "LNyyrkr4e4";
 
 $conn= mysqli_connect($server,$u_name,$password,$db);
 if ($conn){
